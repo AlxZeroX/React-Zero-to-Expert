3c1f5f56ac825803b36531f34e9b2f8b03ec79d8
Cristian Alexander Martinez <40245786+AlxZeroX@users.noreply.github.com>
2022-03-03 12:53:23 -0600

    03-counter-app

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const PrimeraApp = () => {
+
+  return <h1> Hola Mundo </h1>
+
+}
+
+export default PrimeraApp;
---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import './index.css'
+
+const divRoot = document.querySelector('#app');
+
+ReactDOM.render ( <PrimeraApp />, divRoot);
+
