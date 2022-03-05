c89195587abb4a84a74d91a4d2bf7b2982564a56
Cristian_Martinez <thealxzerox@gmail.com>
2022-03-05 12:17:53 -0600

    Memo

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Memorize } from './components/06-memos/Memorize';
 
+//import { Layout } from './components/05-useLayoutEffect/Layout';
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { FocusScreen } from './components/04-useRef/FocusScreen';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
@@ -14,6 +15,6 @@ import { Layout } from './components/05-useLayoutEffect/Layout';
 
 
 ReactDOM.render( 
-    <Layout /> ,
+    <Memorize /> ,
     document.getElementById('root')
 );
