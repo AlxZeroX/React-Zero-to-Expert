a8e2d5bcc384318c0033c1fdef975b4c6e5567fb
Cristian_Martinez <thealxzerox@gmail.com>
2022-03-05 12:30:23 -0600

    useCallback

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MemoHook } from './components/06-memos/MemoHook';
+import { CallbackHook } from './components/06-memos/CallbackHook';
 
+//import { MemoHook } from './components/06-memos/MemoHook';
 //import { Memorize } from './components/06-memos/Memorize';
 //import { Layout } from './components/05-useLayoutEffect/Layout';
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
@@ -16,6 +17,6 @@ import { MemoHook } from './components/06-memos/MemoHook';
 
 
 ReactDOM.render( 
-    <MemoHook /> ,
+    <CallbackHook /> ,
     document.getElementById('root')
 );
