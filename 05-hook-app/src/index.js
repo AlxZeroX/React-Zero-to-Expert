c5d0026b5f436a7b3af2b6a4f9ec7f25c3499e6d
Cristian_Martinez <thealxzerox@gmail.com>
2022-03-05 12:09:17 -0600

    useLayoutEffect

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { Layout } from './components/05-useLayoutEffect/Layout';
 
+//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { FocusScreen } from './components/04-useRef/FocusScreen';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
@@ -13,6 +14,6 @@ import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
 
 ReactDOM.render( 
-    <RealExampleRef /> ,
+    <Layout /> ,
     document.getElementById('root')
 );
