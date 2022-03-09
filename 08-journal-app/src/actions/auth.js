8ab966f808db77c23226631b77338e1a336d6bcd
Cristian Martinez <thealxzerox@gmail.com>
2022-03-09 15:43:36 -0600

    Config Firebase y Google Sign-in

--- 08-journal-app/src/actions/auth.js
+++ 08-journal-app/src/actions/auth.js
@@ -1,10 +1,14 @@
+
+
+
+import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from "../types/types";
 
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
 
         setTimeout(() => {
-            dispatch( login (123, 'Juan') );
+            dispatch(login(123, 'Juan'));
         }, 3500);
     }
 }
@@ -16,3 +20,16 @@ export const login = (uid, displayName) => ({
         displayName
     }
 });
+
+export const startGoogleLogin = () => {
+    return (dispatch) => {
+
+        firebase.auth().signInWithPopup(googleAuthProvider)
+            .then(({ user }) => {
+                dispatch(
+                    login(user.uid, user.displayName)
+                )
+            });
+
+    }
+}
---
+++ 08-journal-app/src/firebase/firebase-config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCW3SwXD1vBbze-atiufUXyHInC6GTj9Ls",
+    authDomain: "react-app-curso-84eb5.firebaseapp.com",
+    projectId: "react-app-curso-84eb5",
+    storageBucket: "react-app-curso-84eb5.appspot.com",
+    messagingSenderId: "863083438657",
+    appId: "1:863083438657:web:cc8c2bb4abd43d64f35cc0"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+
+export {
+    db,
+    googleAuthProvider,
+    firebase
+}
