1bd2ff3c6a392817e7bcf10b1ff1b9e9ad3fb173
Cristian Martinez <thealxzerox@gmail.com>
2022-03-09 15:19:59 -0600

    Thunk Middleware

--- 08-journal-app/src/components/auth/LoginScreen.js
+++ 08-journal-app/src/components/auth/LoginScreen.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 
 
 import { Link } from 'react-router-dom'
-import { login } from '../../actions/auth';
+import { startLoginEmailPassword } from '../../actions/auth';
 import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
@@ -18,7 +18,7 @@ export const LoginScreen = () => {
 
     const handleLogin = (e) => {
         e.preventDefault();
-        dispatch( login( 1234, 'Alex' ) );
+        dispatch( startLoginEmailPassword( email, password ) );
     }
 
     return (
