413e4c3b68f3e2a25b146cd6f9a73ccbb12898fc
Cristian_Martinez <thealxzerox@gmail.com>
2022-03-11 13:12:45 -0600

    React-Big-Calendar

---
+++ calendar-app/src/components/calendar/CalendarScreen.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Navbar } from '../ui/Navbar'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+    {
+      title: "Cumpleaños Mi",
+      start: moment().toDate(),
+      end: moment().add(2, "hours").toDate(),
+      bgcolor: "#fafafa",
+    },
+  ];
+
+
+export const CalendarScreen = () => {
+    return (
+        <div className="calendar-container">
+            <div className="calendar-screen">
+                <Navbar/>
+                
+                <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                />
+            </div>
+        </div>
+    )
+}
---
+++ calendar-app/src/router/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+  } from "react-router-dom";
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+ 
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    
+                    <Route exact path='/login' element={ <LoginScreen /> } />
+ 
+                    <Route path='/' element={ <CalendarScreen /> } />
+ 
+                    <Route path='*' element={<Navigate replace to='/' /> } />
+ 
+                </Routes>
+            </div>
+        </Router>
+    )
+}
