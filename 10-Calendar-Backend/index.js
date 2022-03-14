168e12ea731ee56f4e359190067d61e11c6d7c38
Cristian_Martinez <thealxzerox@gmail.com>
2022-03-14 15:36:08 -0600

    jwt

--- 10-Calendar-Backend/index.js
+++ 10-Calendar-Backend/index.js
@@ -1,23 +1,36 @@
-
-const express = require( 'express');
+const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
 
-// console.log( process.env.PORT );
-
-//Crear el servidor de express
+// Crear el servidor de express
 const app = express();
 
-//Directorio publico
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors())
+
+// Directorio Público
 app.use( express.static('public') );
 
+// Lectura y parseo del body
+app.use( express.json() );
 
-//rutas
+// Rutas
 app.use('/api/auth', require('./routes/auth') );
 // TODO: CRUD: Eventos
 
 
 
-//escuchando peticiones
+// Escuchar peticiones
 app.listen( process.env.PORT, () => {
-    console.log( `Servidor Corriendo en puerto ${ process.env.PORT }`);
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
+
+
+
+
+
+
--- 10-Calendar-Backend/routes/auth.js
+++ 10-Calendar-Backend/routes/auth.js
@@ -2,18 +2,42 @@
     Rutas de Usuarios / Auth
     host + /api/auth
 */
-
-
 const { Router } = require('express');
-const { crearUsuario, loginUsuario, revalidarToken} = require('../controllers/auth');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 
 const router = Router();
 
-router.post( '/new', crearUsuario );
 
-router.post( '/', loginUsuario )
 
-router.get( '/Renew', revalidarToken)
+router.post(
+    '/new', 
+    [ // middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+    ],
+    crearUsuario 
+);
+
+router.post(
+    '/',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+    ],
+    loginUsuario 
+);
+
+
+router.get('/renew', validarJWT ,revalidarToken );
+
+
+
 
 module.exports = router;
