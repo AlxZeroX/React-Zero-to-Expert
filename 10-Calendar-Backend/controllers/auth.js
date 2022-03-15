003c5232c84a0dee2f708b96a53754f1d2bccb8b
Cristian_Martinez <thealxzerox@gmail.com>
2022-03-15 15:27:01 -0600

    +changes

--- 10-Calendar-Backend/controllers/auth.js
+++ 10-Calendar-Backend/controllers/auth.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 const { generarJWT } = require('../helpers/jwt');
  
