e090a39e12be32e3654975748a963e947a27ff71
Cristian Martinez <thealxzerox@gmail.com>
2022-03-16 12:46:08 -0600

    auth update

--- 09-Calendar-app/src/router/AppRouter.js
+++ 09-Calendar-app/src/router/AppRouter.js
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     BrowserRouter as Router,
     Routes,
     Route,
     Navigate
-  } from "react-router-dom";
+} from "react-router-dom";
+
+import { useDispatch, useSelector } from 'react-redux';
+
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
- 
+import { startChecking } from '../actions/auth';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
+
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+    const { checking, uid } = useSelector(state => state.auth);
+
+    useEffect(() => {
+
+        dispatch(startChecking());
+
+    }, [dispatch])
+
+    if (checking) {
+        return (<h5>Espere...</h5>);
+    }
+
     return (
         <Router>
             <div>
                 <Routes>
-                    
-                    <Route exact path='/login' element={ <LoginScreen /> } />
- 
-                    <Route path='/' element={ <CalendarScreen /> } />
- 
-                    <Route path='*' element={<Navigate replace to='/' /> } />
- 
+
+                    <PublicRoute
+                        exact
+                        path="/login"
+                        component={LoginScreen}
+                        isAuthenticated={!!uid}
+                    />
+
+                    <PrivateRoute
+                        exact
+                        path="/"
+                        component={CalendarScreen}
+                        isAuthenticated={!!uid}
+                    />
+
+                    <Route path='*' element={<Navigate replace to='/' />} />
+
                 </Routes>
             </div>
         </Router>
